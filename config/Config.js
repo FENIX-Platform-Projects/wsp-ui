398ed1b9687468dad364b5e2b26b025cb4868dc3
simonefenix <simone.murzilli@fao.org>
2015-06-19 17:53:47 +0200

    configs

--- config/Config.js
+++ config/Config.js
@@ -5,10 +5,5 @@ define(function ( ) {
 
     return {
 
-        //WDS and Data
-        DB_NAME : 'flude',
-        WDS_URL : 'http://fenixapps2.fao.org/wds_5/rest/fenix/query',
-        WDS_OUTPUT_TYPE : 'object'
-
     };
 });
--- config/Services.js
+++ config/Services.js
@@ -4,13 +4,14 @@ define(function ( ) {
     'use strict';
 
     return {
-        CL_COUNTRY: "SELECT * FROM test",
-        CL_YEAR: "SELECT * FROM test",
-        CL_INCOMES: "SELECT * FROM test",
-        CL_SUBREGION: "SELECT * FROM test",
-        CL_DOMAIN: "SELECT * FROM test",
-        CL_INDICATORS: "SELECT * FROM test",
 
-        DATA: "SELECT * FROM test"
+        "url_geoserver_wms": "http://fenix.fao.org/geoserver",
+        "url_geoserver_wms_demo": "http://fenix.fao.org/demo/fenix/geoserver",
+        "url_d3s_metadata_resource": "http://fenix.fao.org/d3s/msd/resources/uid/{{UID}}",
+        "url_d3s_resources_find": "http://fenix.fao.org/d3s/msd/resources/find",
+        "url_d3s_resources_find_order_by_date_parameters": "order=meContent.seCoverage.coverageTime.from:desc&logic=StandardFilter&dsd=true&full=true",
+
+        "url_geostatistics_rasters_pixel": "http://fenix.fao.org/demo/fenix/geo/geostatistics/rasters/pixel/"
+
     };
 });
